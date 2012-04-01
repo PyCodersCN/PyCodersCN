e0ddbb8b703bdd33e217b4b5c926a5f14581ec7b
Zeray Rice <fanzeyi1994@gmail.com>
2012-04-01 13:52:36 +0800

    修改主题样式

--- conf.py
+++ conf.py
@@ -102,7 +102,9 @@ highlight_language = 'python'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid'
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'pycoders'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
